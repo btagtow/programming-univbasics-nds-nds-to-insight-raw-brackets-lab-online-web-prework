35e4d0e8742dbd441d3896fe3831c0db9fee6e17
Ben Tagtow <bentagtow@gmail.com>
2019-11-11 18:50:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,23 +1,17 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-require 'pp'
 def directors_totals(nds)
-  movie_total={}
-  row_index=0 
-  while row_index<nds.length do 
-    column_index=0 
-    while column_index<nds[row_index].length do 
-      inner_len=nds[row_index][column_index]
-      inner_index=0 
-      while inner_index<inner_len do 
-        movie_total={nds[row_index]=>nds[row_index][column_index][inner_index][:worldwide_gross]}
-        inner_index+=1
-      end 
-      column_index+=1
-    end 
-    row_index+=1
-  end 
-  return movie_total
+  result = {}
+  index=0 
+  totals={} 
+  while i<nds.length do 
+    directors_name=nds[index][:name]
+    totals[directors_name]=0 
+    movie_index=0 
+    while movie_index<nds[i][:movies][movie_index
+    ][:worldwide_gross]
+    movie_index
+
 end 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
